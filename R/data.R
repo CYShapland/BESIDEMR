24b107729aed226ef2b96ae27fbeece05947c373
Chin Yang Shapland <ew18103@bristol.ac.uk>
2021-08-26 16:51:34 +0100

    Include MRChallenge2019 data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Effect of HDL choesterol on age-related macular degeneration (AMD)
+#'
+#' We performed three-sample MR, where we selected instruments from The Metabolic Syndrome in Men (METSIM) GWAS, then extracted their summary data from `MRChallenge2019`, where:
+#' \enumerate{
+#' \item{SNP-HDL:}{Kettunen et al. Available from: http://europepmc.org/articles/PMC4814583}
+#' \item{SNP-AMD:}{International AMD Genomics Consortium (IAMDGC) Available from: http://europepmc.org/articles/PMC4745342}
+#' }
+#'
+#'
+#' @docType data
+#'
+#' @usage data(amd.hdl)
+#'
+#' @format A \code{data.frame} with 27 rows and 4 variables.
+#'
+#' @keywords datasets
+#'
+#'
+"amd.hdl"
