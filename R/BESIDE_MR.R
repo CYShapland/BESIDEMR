6bd09c69f27c722950b3deb1132e6adc5e2c68dc
Chin Yang Shapland <ew18103@bristol.ac.uk>
2021-08-27 10:45:57 +0100

    Correcting real data example

--- R/BESIDE_MR.R
+++ R/BESIDE_MR.R
@@ -475,6 +475,12 @@ BMA_MRanalysis<-function(tau_estimate, N_Beta, BetaXG,BetaYG,seBetaXG,seBetaYG, 
 #' @export
 #' @examples
 #'
+#'
+#' #load data
+#' data(AMD_HDL)
+#'
+#' #Prior choice for beta, tau and inclusion of instruments
+#' L<-nrow(AMD_HDL)
 #' Ins_prior<-rep(0.5, L)
 #' randomS.initial.LI(L,Ins_prior)
 
